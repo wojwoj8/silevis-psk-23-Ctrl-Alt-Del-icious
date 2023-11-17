7881689889e60d2cf2552b9a5375db763366eddb
matorolll <mgajos00@gmail.com>
2023-11-17 22:44:41 +0100

    Adding hub and others

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import BannerComponent from './components/BannerComponent'
+import NavigationComponent from './components/NavigationComponent'
+import HubComponent from './components/hubComponent'
+
 
 function App() {
  const client = ""
   return (
     <>
       <Header/>
+      <NavigationComponent/>
+      <BannerComponent/>
+      <HubComponent/>
       <Outlet/>
       <Footer/>
     </>
---
+++ frontend/src/components/NavigationComponent.tsx
@@ -0,0 +1,8 @@
+export default function BannerComponent(){
+    return(
+        <div className=" d-flex justify-content-start text-orange5">
+            <div className="h3  ps-5 m-4">  WEAiI</div>
+
+        </div>
+    )
+}
