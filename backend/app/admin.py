bf39450a09c31f9adebd2958b3c1a73b8a1423a9
wojwoj8 <wojwoj8@gmail.com>
2023-11-17 20:44:43 +0100

    attatchment1 backend

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from app.models import (
+    User,
+    Attachment1,
+)
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Attachment1)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "corsheaders",
+    "app",
     "rest_framework",
 ]
 
