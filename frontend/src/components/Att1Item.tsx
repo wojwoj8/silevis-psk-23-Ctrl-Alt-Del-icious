f742f8ab7e6a77b4610cfaa267f62b988fed954e
wojwoj8 <wojwoj8@gmail.com>
2023-11-18 06:01:11 +0100

    bugfix

--- frontend/src/components/Att1Item.tsx
+++ frontend/src/components/Att1Item.tsx
@@ -12,11 +12,7 @@ const Att1Item = ({data}) =>{
 
             <Link to={`/umowapdf/${data.id}`}>
             <div className={`border border-black ${data.status === 'accepted' ? 'bg-success' : data.status === 'denied' ? 'bg-danger' : data.status === 'pending' ? 'bg-primary' : ''}`}>
-<<<<<<< HEAD
                 {data.id}
-=======
-            {data.id}
->>>>>>> backend
             </div>
             </Link>
 
