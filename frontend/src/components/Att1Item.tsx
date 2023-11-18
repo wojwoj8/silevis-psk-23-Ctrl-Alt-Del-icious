8421463af0e76dcdfc927d794b5564ea0bfcf419
matorolll <mgajos00@gmail.com>
2023-11-18 12:23:36 +0100

    Changing adminatt

--- frontend/src/components/Att1Item.tsx
+++ frontend/src/components/Att1Item.tsx
@@ -12,7 +12,7 @@ const Att1Item = ({data, defaultData}) =>{
             <Link to={`/umowapdf/${data.id}`}>
             <div className={`border border-black ${data.status === 'accepted' ? 'bg-success' : data.status === 'denied' ? 'bg-danger' : data.status === 'pending' ? 'bg-primary' : ''}`}>
             {data.start_praktyk !== (defaultData && defaultData.start_praktyk) ?
-                <p>Unusual or no data</p> :
+                <p className="text-dark link-underline link-underline-opacity-0 pt-3">Unusual data</p> :
                 <p className="text-dark link-underline link-underline-opacity-0 pt-3">{data.nr_albumu}</p>
             }
             </div>
