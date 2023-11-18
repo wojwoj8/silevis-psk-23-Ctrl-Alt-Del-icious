127f6e4428f1bcb93c45e4a86f0d7235cd7bf25a
matorolll <mgajos00@gmail.com>
2023-11-18 15:04:32 +0100

    Changing language

--- frontend/src/components/BannerComponent.tsx
+++ frontend/src/components/BannerComponent.tsx
@@ -1,12 +1,18 @@
+import LanguageContext from '../context/LanguageContext'
+import { useContext } from 'react';
+
 export default function BannerComponent(){
+    const {language} = useContext(LanguageContext)
+
     return(
         <div className="banner d-flex justify-content-evenly h-32">
             <div className="h4 text-white m-5">
-                Praktyki - profil akademicki
+                {language=='pl'?'Praktyki - profil akademicki':'Internships - academic profile'}
+
             </div>
             <div></div>
             <div className="h4 text-uppercase text-danger m-5">
-                porażamy pozytywną energią
+            {language=='pl'?'porażamy pozytywną energią':'we strike with positive energy'}
             </div>
         </div>
     )
