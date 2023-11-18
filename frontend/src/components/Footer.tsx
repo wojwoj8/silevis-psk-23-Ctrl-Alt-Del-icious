9d04b1b22ec9450809a3de938471d631d2783b98
matorolll <mgajos00@gmail.com>
2023-11-18 16:25:35 +0100

    fixing footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
   return  (
-  <footer className="d-flex justify-content-center orange5 text-white p-5">
+  <footer className="d-flex justify-content-center orange5 text-white p-5 mt-60">
       <p>© 2023 Hackaton, Ctrl-Alt-Del-icious, All rights reserved.</p>
       
 </footer>
