f483f1d2038acb2e42b5e4e47b30fbb53cb315b0
matorolll <mgajos00@gmail.com>
2023-11-18 13:13:18 +0100

    Changing style att1items

--- frontend/src/components/Att1Item.tsx
+++ frontend/src/components/Att1Item.tsx
@@ -1,23 +1,16 @@
-import { useState, useEffect } from 'react';
-import axios, { AxiosError } from 'axios';
 import { Link } from 'react-router-dom';
 
 const Att1Item = ({data, defaultData}) =>{
-
-    
     return(
-
-
-        <div className='container'>
+        <div className='container pb-1'>
             <Link to={`/umowapdf/${data.id}`}>
-            <div className={`border border-black ${data.status === 'accepted' ? 'bg-success' : data.status === 'denied' ? 'bg-danger' : data.status === 'pending' ? 'bg-primary' : ''}`}>
-            {data.start_praktyk !== (defaultData && defaultData.start_praktyk) ?
-                <p className="text-dark link-underline link-underline-opacity-0 pt-3">Unusual data</p> :
-                <p className="text-dark link-underline link-underline-opacity-0 pt-3">{data.nr_albumu}</p>
-            }
-            </div>
+                <div className={`border border-black ${data.status === 'accepted' ? 'acceptedbg' : data.status === 'denied' ? 'deniedbg' : data.status === 'pending' ? 'pendingbg' : ''}`}>
+                {data.start_praktyk !== (defaultData && defaultData.start_praktyk) ?
+                    <p className="text-dark link-underline link-underline-opacity-0 pt-3">Unusual data</p> :
+                    <p className="text-dark pt-3">{data.nr_albumu}</p>
+                }
+                </div>
             </Link>
-
         </div>
     )
 }
